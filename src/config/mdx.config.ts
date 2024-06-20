0a8d46a3c880752a34349cbaf8f5d2d8a784bc54
Stefan Probst <stefanprobst@posteo.de>
2024-06-20 16:04:19 +0200

    chore: align with template

--- src/config/mdx.config.ts
+++ src/config/mdx.config.ts
@@ -1,7 +1,7 @@
 import { join } from "node:path";
 
 import type { CompileOptions } from "@mdx-js/mdx";
-import withSyntaxHighlighter from "@shikijs/rehype";
+// import withSyntaxHighlighter from "@shikijs/rehype";
 import withAssets from "rehype-mdx-import-media";
 import withHeadingIds from "rehype-slug";
 import withFrontmatter from "remark-frontmatter";
@@ -11,9 +11,9 @@ import withTypographicQuotes from "remark-smartypants";
 import type { Options as TypographicOptions } from "retext-smartypants";
 
 import type { Locale } from "@/config/i18n.config";
-import { config as syntaxHighlighterConfig } from "@/config/syntax-highlighter.config";
-import { withMdxFootnotes } from "@/lib/content/footnotes";
-import { withMdxTableOfContents, withTableOfContents } from "@/lib/content/table-of-contents";
+// import { config as syntaxHighlighterConfig } from "@/config/syntax-highlighter.config";
+// import { withMdxFootnotes } from "@/lib/content/footnotes";
+// import { withMdxTableOfContents, withTableOfContents } from "@/lib/content/table-of-contents";
 import { createI18n } from "@/lib/i18n";
 
 const cache = new Map<Locale, CompileOptions>();
@@ -50,11 +50,11 @@ export async function createConfig(locale: Locale) {
 			footnoteLabel: t("Mdx.Footnotes"),
 		},
 		rehypePlugins: [
-			withMdxFootnotes,
+			// withMdxFootnotes,
 			withHeadingIds,
-			withTableOfContents,
-			withMdxTableOfContents,
-			[withSyntaxHighlighter, syntaxHighlighterConfig],
+			// withTableOfContents,
+			// withMdxTableOfContents,
+			// [withSyntaxHighlighter, syntaxHighlighterConfig],
 			withAssets,
 		],
 	};

da33b91172f1c6d5ad3da187740a4a56fc964301
Stefan Probst <stefanprobst@posteo.de>
2024-08-17 08:12:48 +0200

    chore: remove unused settings

--- astro.config.ts
+++ astro.config.ts
@@ -22,11 +22,6 @@ export default defineConfig({
 		mode: "standalone",
 	}),
 	base: env.PUBLIC_APP_BASE_PATH,
-	experimental: {
-		// actions: true,
-		contentCollectionCache: true,
-		rewriting: true,
-	},
 	integrations: [
 		icon({
 			/** @see https://www.astroicon.dev/reference/configuration/#include */
